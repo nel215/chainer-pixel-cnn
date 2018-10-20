e8cf4657ebbba794586b2cb29634f5d4f68279cd
nel215 <otomo.yuhei@gmail.com>
2018-10-20 17:01:23 +0900

    Add Deconv layers

--- pixel_cnn/model/conv2d.py
+++ pixel_cnn/model/conv2d.py
@@ -34,3 +34,33 @@ class DownShiftedConv2D(Chain):
         h = F.pad(x, self.pad, 'constant')
         h = self.conv2d(h)
         return h
+
+
+class DownShiftedDeconv2D(Chain):
+
+    def __init__(self, n_out, ksize):
+        super(DownShiftedDeconv2D, self).__init__()
+        self.ksize = ksize
+        with self.init_scope():
+            self.deconv2d = L.Deconvolution2D(None, n_out, ksize=ksize)
+
+    def __call__(self, x):
+        h = self.deconv2d(x)
+        s1 = slice(0, -self.ksize[0]+1)
+        s2 = slice((self.ksize[1]-1)//2, -(self.ksize[1]-1)//2)
+        return h[:, :, s1, s2]
+
+
+class DownRightShiftedDeconv2D(Chain):
+
+    def __init__(self, n_out, ksize):
+        super(DownRightShiftedDeconv2D, self).__init__()
+        self.ksize = ksize
+        with self.init_scope():
+            self.deconv2d = L.Deconvolution2D(None, n_out, ksize=ksize)
+
+    def __call__(self, x):
+        h = self.deconv2d(x)
+        s1 = slice(0, -self.ksize[0]+1)
+        s2 = slice(0, -self.ksize[1]+1)
+        return h[:, :, s1, s2]
--- pixel_cnn/model/test_conv2d.py
+++ pixel_cnn/model/test_conv2d.py
@@ -1,5 +1,8 @@
 import numpy as np
-from .conv2d import DownShiftedConv2D, DownRightShiftedConv2D
+from .conv2d import (
+    DownShiftedConv2D, DownRightShiftedConv2D,
+    DownShiftedDeconv2D, DownRightShiftedDeconv2D,
+)
 
 
 def test_down_call():
@@ -18,5 +21,25 @@ def test_down_right_call():
     n_out = 8
     model = DownRightShiftedConv2D(n_out, (2, 1))
     x = np.zeros((b, 3, n, n), dtype='f')
+    h = model(x)
+    assert h.shape == (b, n_out, n, n)
+
+
+def test_down_deconv_call():
+    b = 4
+    n = 16
+    n_out = 8
+    model = DownShiftedDeconv2D(n_out, (2, 3))
+    x = np.zeros((b, 16, n, n), dtype='f')
+    h = model(x)
+    assert h.shape == (b, n_out, n, n)
+
+
+def test_down_right_deconv_call():
+    b = 4
+    n = 16
+    n_out = 8
+    model = DownRightShiftedDeconv2D(n_out, (2, 2))
+    x = np.zeros((b, 16, n, n), dtype='f')
     h = model(x)
     assert h.shape == (b, n_out, n, n)
