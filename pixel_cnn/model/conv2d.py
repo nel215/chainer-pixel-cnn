3579978498e3be73ce7ac4c25c0672d9deda49b4
nel215 <otomo.yuhei@gmail.com>
2018-10-20 15:55:17 +0900

    Add DownShiftedConv2d

---
+++ pixel_cnn/model/conv2d.py
@@ -0,0 +1,20 @@
+from chainer import Chain
+from chainer import links as L
+from chainer import functions as F
+
+
+class DownShiftedConv2D(Chain):
+
+    def __init__(self, n_out, ksize):
+        super(DownShiftedConv2D, self).__init__()
+        self.pad = [
+            (0, 0), (0, 0),
+            (ksize[0]-1, 0), ((ksize[1]-1)//2, (ksize[1]-1)//2),
+        ]
+        with self.init_scope():
+            self.conv2d = L.Convolution2D(None, n_out, ksize=ksize)
+
+    def __call__(self, x):
+        h = F.pad(x, self.pad, 'constant')
+        h = self.conv2d(h)
+        return h
